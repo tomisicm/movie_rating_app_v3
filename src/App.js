6009a112c584cd8c1b9f5422d8445c4a4580dbd2
Mihailo Tomisic <mihailotomisic@MacBook-Pro-6.local>
2019-05-13 23:18:48 +0200

    focus

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
 import React from "react"
 import "./App.css"
 
+import "bootstrap/dist/css/bootstrap.min.css"
+
 function App() {
-  return (
-    <div className="container">
-      <div className="row">
-        <div className="col-sm">One of three columns</div>
-        <div className="col-sm">One of three columns</div>
-        <div className="col-sm">One of three columns</div>
-      </div>
-    </div>
-  )
+  return <div className="container">App</div>
 }
 
 export default App
