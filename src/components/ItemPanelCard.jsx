b883e94fef8dc0bbe97290d8358b6c713735aef0
Mihailo Tomisic <mihailotomisic@MacBook-Pro-6.local>
2019-05-14 15:09:35 +0200

    stying ItemPanelCard

--- src/components/ItemPanelCard.jsx
+++ src/components/ItemPanelCard.jsx
@@ -7,19 +7,27 @@ class ItemCard extends Component {
 
   render() {
     return (
-      <div class="card">
-        <div class="card-body">
-          <img
+      <div className="card border-secondary">
+        <div
+          className="card-body"
+          style={{
+            backgroundImage: `url(${this.state.src})`,
+            height: "110px"
+          }}
+        >
+          {/* <img
             className="card-img-top img-fluid"
             src={this.state.src}
             alt="movieImg"
-          />
+          /> */}
         </div>
-        <div style={{ marginLeft: "22px" }} className="item-rating m*-2 p*-2">
-          <span className="card-text">movie nameeee must two lines</span>
-          <div class="row">
-            <div class="col-sm">Rating</div>
-            <div class="col-sm">Comments</div>
+        <div className="item-rating">
+          <h6 className="card-title pl-2">movie nameeee must two lines</h6>
+
+          <div className="dropdown-divider" />
+          <div className="row card-text">
+            <div className="col pb-2 ml-2">Rating: 2.3</div>
+            <div className="col pb-2 ml-1">Comments: 234</div>
           </div>
         </div>
       </div>
--- src/views/Dashboard.jsx
+++ src/views/Dashboard.jsx
@@ -6,29 +6,29 @@ class Dashboard extends Component {
   state = {}
   render() {
     return (
-      <div className="content m*-4 mx-2">
+      <div className="content">
         <div className="container mx-auto">
           <h1 className="my-4">The most recent movies</h1>
           <div className="movie-panel">
-            <div class="row">
-              <div class="col-sm">
+            <div className="row">
+              <div className="col">
                 <ItemPanelCard />
               </div>
-              <div class="col-sm">
+              <div className="col">
                 <ItemPanelCard />
               </div>
-              <div class="col-sm">
+              <div className="col">
                 <ItemPanelCard />
               </div>
             </div>
-            <div class="row">
-              <div class="col-sm">
+            <div className="row">
+              <div className="col-sm">
                 <ItemPanelCard />
               </div>
-              <div class="col-sm">
+              <div className="col-sm">
                 <ItemPanelCard />
               </div>
-              <div class="col-sm">
+              <div className="col-sm">
                 <ItemPanelCard />
               </div>
             </div>
