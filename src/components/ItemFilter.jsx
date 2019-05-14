7d13ac92bab8120bbfd2954ee925af73e004ace2
Mihailo Tomisic <mihailotomisic@MacBook-Pro-6.local>
2019-05-14 17:32:06 +0200

    release year filter

--- src/components/ItemFilter.jsx
+++ src/components/ItemFilter.jsx
@@ -2,8 +2,37 @@ import React, { Component } from "react"
 
 class MainItemFilter extends Component {
   state = {}
+  onChange = e => {
+    console.log(e.target.value)
+  }
+
   render() {
-    return <div>dis item filt</div>
+    return (
+      <React.Fragment>
+        <div className="form-group">
+          <label htmlFor="film-name">Film name:</label>
+          <input
+            className="form-control"
+            id="film-name"
+            aria-describedby="emailHelp"
+            placeholder="Enter film name"
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="film-name">Release Year:</label>
+          <div className="container">
+            <input
+              type="range"
+              className="custom-range"
+              id="ReleaseYear"
+              onChange={this.onChange}
+            />
+            <span className="col indigo-text mt-1">1950</span>
+            <span className="col mr-auto indigo-text mt-1">2019</span>
+          </div>
+        </div>
+      </React.Fragment>
+    )
   }
 }
 
