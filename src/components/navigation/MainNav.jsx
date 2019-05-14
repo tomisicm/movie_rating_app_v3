bfd7e2dfe7584862f26e502c6955a6f7ed2229b0
Mihailo Tomisic <mihailotomisic@MacBook-Pro-6.local>
2019-05-14 11:56:38 +0200

    wirin up navigation

--- src/components/navigation/MainNav.jsx
+++ src/components/navigation/MainNav.jsx
@@ -2,8 +2,8 @@ import React from "react"
 import { Link, NavLink } from "react-router-dom"
 
 const MainNav = props => {
-  // const user = { name: "djura" }
-  const user = null
+  const user = { name: "djura" }
+  //const user = null
 
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
@@ -24,39 +24,50 @@ const MainNav = props => {
               <button
                 className="btn btn-secondary dropdown-toggle"
                 type="button"
-                id="dropdownMenu2"
                 data-toggle="dropdown"
                 aria-haspopup="true"
                 aria-expanded="false"
               >
-                Dropdown
+                User
               </button>
-              <div className="dropdown-menu" aria-labelledby="dropdownMenu2">
-                <button className="dropdown-item" type="button">
-                  Action
-                </button>
-                <button className="dropdown-item" type="button">
-                  Another action
-                </button>
-                <button className="dropdown-item" type="button">
-                  Something else here
-                </button>
+              <div className="dropdown-menu">
+                <li className="nav-item">
+                  <NavLink className="nav-link" to="/login">
+                    Login
+                  </NavLink>
+                </li>
+                <li className="nav-item">
+                  <NavLink className="nav-link" to="/register">
+                    Register
+                  </NavLink>
+                </li>
               </div>
             </div>
           )}
           {user && (
-            <React.Fragment>
-              <li className="nav-item">
-                <NavLink className="nav-link" to="/profile">
-                  {user.name}
-                </NavLink>
-              </li>
-              <li className="nav-item">
-                <NavLink className="nav-link" to="/logout">
-                  Logout
-                </NavLink>
-              </li>
-            </React.Fragment>
+            <div className="dropdown">
+              <button
+                className="btn btn-secondary dropdown-toggle"
+                type="button"
+                data-toggle="dropdown"
+                aria-haspopup="true"
+                aria-expanded="false"
+              >
+                {user.name}
+              </button>
+              <div className="dropdown-menu">
+                <li className="nav-item">
+                  <NavLink className="nav-link" to="/profile">
+                    Profile
+                  </NavLink>
+                </li>
+                <li className="nav-item">
+                  <NavLink className="nav-link" to="/logout">
+                    Logout
+                  </NavLink>
+                </li>
+              </div>
+            </div>
           )}
         </ul>
       </div>
