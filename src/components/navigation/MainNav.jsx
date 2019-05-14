1e7e510d3528a8c797f4ce211419b9c507d7b8f0
Mihailo Tomisic <mihailotomisic@MacBook-Pro-6.local>
2019-05-14 09:32:56 +0200

    removing colapse

--- src/components/navigation/MainNav.jsx
+++ src/components/navigation/MainNav.jsx
@@ -2,12 +2,12 @@ import React from "react"
 import { Link, NavLink } from "react-router-dom"
 
 const MainNav = props => {
-  const user = { name: "djura" }
-  // const user = null
+  // const user = { name: "djura" }
+  const user = null
 
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <div class="navbar-collapse collapse justify-content-between">
+      <div class="navbar-collapse justify-content-between">
         <ul class="navbar-nav mr-auto">
           <NavLink className="navbar-brand" to="/dashboard">
             Dashboard
