c73bac3e1ba6671c9c6c4d2bd57bc3c1603add54
Mihailo Tomisic <mihailotomisic@MacBook-Pro-6.local>
2019-05-14 19:08:14 +0200

    select

--- src/components/ItemFilter.jsx
+++ src/components/ItemFilter.jsx
@@ -3,13 +3,27 @@ import React, { Component } from "react"
 import { Range } from "rc-slider"
 import "rc-slider/assets/index.css"
 
+import Select from "react-select"
+
+const options = [
+  { value: "chocolate", label: "Chocolate" },
+  { value: "strawberry", label: "Strawberry" },
+  { value: "vanilla", label: "Vanilla" }
+]
+
 class MainItemFilter extends Component {
   state = {
-    releaseYear: [2000, new Date().getFullYear()]
+    releaseYear: [2000, new Date().getFullYear()],
+    selectedOption: null
   }
   onChange = e => {
     const state = { ...this.state }
     this.setState({ ...state, releaseYear: e })
+  }
+
+  handleChange = selectedOption => {
+    this.setState({ selectedOption })
+    console.log(`Option selected:`, selectedOption)
   }
 
   render() {
@@ -41,6 +55,17 @@ class MainItemFilter extends Component {
             ]}
             allowCross={false}
             onChange={this.onChange}
+          />
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="film-name">Genres</label>
+
+          <Select
+            value={this.state.selectedOption}
+            onChange={this.handleChange}
+            options={options}
+            isMulti={true}
           />
         </div>
       </React.Fragment>
