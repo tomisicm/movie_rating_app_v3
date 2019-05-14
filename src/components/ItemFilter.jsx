dcd05bf49b9c4ff1a48fc721d0167bf255bb676c
Mihailo Tomisic <mihailotomisic@MacBook-Pro-6.local>
2019-05-14 16:44:06 +0200

    item filter layout

---
+++ src/components/ItemFilter.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from "react"
+
+class MainItemFilter extends Component {
+  state = {}
+  render() {
+    return <div>dis item filt</div>
+  }
+}
+
+export default MainItemFilter
--- src/views/MoviesPage.jsx
+++ src/views/MoviesPage.jsx
@@ -1,9 +1,22 @@
 import React, { Component } from "react"
 
+import ItemFilter from "../components/ItemFilter"
+
 class MoviesPage extends Component {
   state = {}
   render() {
-    return <div>This is movies page</div>
+    return (
+      <div className="content">
+        <div className="container mx-auto">
+          <div className="row">
+            <div className="col-3">
+              <ItemFilter />
+            </div>
+            <div className="col-9">Movie list</div>
+          </div>
+        </div>
+      </div>
+    )
   }
 }
 
